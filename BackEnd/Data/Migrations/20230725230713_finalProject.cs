fb7d7bfedd5d5fda503d03dc913070e812a76aa3
Sandeep Singh <sjohal815@gmail.com>
2023-07-25 17:53:30 -0600

    finalProject

--- BackEnd/Data/Migrations/20230725230713_finalProject.cs
+++ BackEnd/Data/Migrations/20230725230713_finalProject.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Data.Migrations
 {
-    public partial class IntiialCreate : Migration
+    public partial class finalProject : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -224,17 +224,17 @@ namespace Data.Migrations
             migrationBuilder.InsertData(
                 table: "AspNetRoles",
                 columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
-                values: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7210", "ed205d13-89aa-4a82-8161-ca0a6078a03e", "Capstone Todos Admin", "Admin", "ADMIN" });
+                values: new object[] { "2c5e174e-3b0e-446f-86af-483d56fd7210", "3b8ffd24-b70a-40bb-86f6-254033907f58", "Capstone Todos Admin", "Admin", "ADMIN" });
 
             migrationBuilder.InsertData(
                 table: "AspNetRoles",
                 columns: new[] { "Id", "ConcurrencyStamp", "Description", "Name", "NormalizedName" },
-                values: new object[] { "65764886-4f92-4c2d-b426-a0fe8b26e855", "471c7ef2-c361-42a8-add1-d141061de394", "Capstone Todos User", "User", "USER" });
+                values: new object[] { "65764886-4f92-4c2d-b426-a0fe8b26e855", "10295230-8cf0-4490-bb23-be39e33148c2", "Capstone Todos User", "User", "USER" });
 
             migrationBuilder.InsertData(
                 table: "AspNetUsers",
                 columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
-                values: new object[] { "8e445865-a24d-4543-a6c6-9443d048cdb9", 0, "daa54e97-7f6d-4264-93d6-caa36395522e", "admin@capstone.com", true, "Admin", "Capstone", false, null, "admin@capstone.com", "admin@capstone.com", "AQAAAAEAACcQAAAAEKbuh7eSrT/N4qSReLNscO5uccDlF/RRV5QZMEL99n4RJhYzHSgDK7WXuoyl08Zu8w==", "17809091212", false, "9880e79b-41cf-4b02-b781-46258de51980", false, "admin@capstone.com" });
+                values: new object[] { "8e445865-a24d-4543-a6c6-9443d048cdb9", 0, "d01c7864-75b0-44b8-b43e-82f67ddad6f6", "admin@capstone.com", true, "Admin", "Capstone", false, null, "admin@capstone.com", "admin@capstone.com", "AQAAAAEAACcQAAAAEJ40IdzxzguTyjlv6etOLXUqT34+Y9P8HTPNZA5flcM+r+5hwYmLHuFDrnwPZhe0gQ==", "17809091212", false, "017bbba5-4d9e-484e-a2ad-705d5a74f2b2", false, "admin@capstone.com" });
 
             migrationBuilder.InsertData(
                 table: "AspNetUserRoles",
--- BackEnd/Data/Migrations/ApiDataContextModelSnapshot.cs
+++ BackEnd/Data/Migrations/ApiDataContextModelSnapshot.cs
@@ -102,7 +102,7 @@ namespace Data.Migrations
                         new
                         {
                             Id = "2c5e174e-3b0e-446f-86af-483d56fd7210",
-                            ConcurrencyStamp = "ed205d13-89aa-4a82-8161-ca0a6078a03e",
+                            ConcurrencyStamp = "3b8ffd24-b70a-40bb-86f6-254033907f58",
                             Description = "Capstone Todos Admin",
                             Name = "Admin",
                             NormalizedName = "ADMIN"
@@ -110,7 +110,7 @@ namespace Data.Migrations
                         new
                         {
                             Id = "65764886-4f92-4c2d-b426-a0fe8b26e855",
-                            ConcurrencyStamp = "471c7ef2-c361-42a8-add1-d141061de394",
+                            ConcurrencyStamp = "10295230-8cf0-4490-bb23-be39e33148c2",
                             Description = "Capstone Todos User",
                             Name = "User",
                             NormalizedName = "USER"
@@ -231,7 +231,7 @@ namespace Data.Migrations
                         {
                             Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
                             AccessFailedCount = 0,
-                            ConcurrencyStamp = "daa54e97-7f6d-4264-93d6-caa36395522e",
+                            ConcurrencyStamp = "d01c7864-75b0-44b8-b43e-82f67ddad6f6",
                             Email = "admin@capstone.com",
                             EmailConfirmed = true,
                             FirstName = "Admin",
@@ -239,10 +239,10 @@ namespace Data.Migrations
                             LockoutEnabled = false,
                             NormalizedEmail = "admin@capstone.com",
                             NormalizedUserName = "admin@capstone.com",
-                            PasswordHash = "AQAAAAEAACcQAAAAEKbuh7eSrT/N4qSReLNscO5uccDlF/RRV5QZMEL99n4RJhYzHSgDK7WXuoyl08Zu8w==",
+                            PasswordHash = "AQAAAAEAACcQAAAAEJ40IdzxzguTyjlv6etOLXUqT34+Y9P8HTPNZA5flcM+r+5hwYmLHuFDrnwPZhe0gQ==",
                             PhoneNumber = "17809091212",
                             PhoneNumberConfirmed = false,
-                            SecurityStamp = "9880e79b-41cf-4b02-b781-46258de51980",
+                            SecurityStamp = "017bbba5-4d9e-484e-a2ad-705d5a74f2b2",
                             TwoFactorEnabled = false,
                             UserName = "admin@capstone.com"
                         });
